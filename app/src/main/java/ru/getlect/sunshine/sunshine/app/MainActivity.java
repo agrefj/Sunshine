d0873f1ef908cf5bdf31f6c76dad3e5a61a44d7c
agrefj <agrefj@gmail.com>
2015-04-10 21:24:34 +0300

    1.06

--- app/src/main/java/ru/getlect/sunshine/sunshine/app/MainActivity.java
+++ app/src/main/java/ru/getlect/sunshine/sunshine/app/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -87,8 +88,9 @@ public class MainActivity extends ActionBarActivity {
                     R.layout.list_item_forecast,
                     weekForecast);
 
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
 
-          
             return rootView;
         }
     }
