b2eaaeb043b3adc2368c4d8e27544f8023fa6908
agrefj <agrefj@gmail.com>
2015-04-15 21:23:23 +0300

    2.06
    Add internet permissions

--- app/src/main/java/ru/getlect/sunshine/sunshine/app/ForecastFragment.java
+++ app/src/main/java/ru/getlect/sunshine/sunshine/app/ForecastFragment.java
@@ -137,6 +137,9 @@ public class ForecastFragment extends Fragment {
                         return null;
                     }
                     forecastJsonStr = buffer.toString();
+
+                    Log.v(LOG_TAG, "Forecast JSON String: " + forecastJsonStr );
+
                 } catch (IOException e) {
                     Log.e("PlaceholderFragment", "Error ", e);
                     // If the code didn't successfully get the weather data, there's no point in attempting
